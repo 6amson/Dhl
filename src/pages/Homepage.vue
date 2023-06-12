2b834a1362abab24424a0007da574c38a5dcf253
bunmigrey <bunmigrey@icloud.com>
2023-06-12 23:30:39 +0100

    2/4 homepage done

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -50,49 +50,63 @@
       </div>
 
       <div class="statDiv">
-          <div class="statItem firstStatDiv">
-              <p>120k</p>
-              <p>Cities</p>
-          </div>
+        <div class="statItem firstStatDiv">
+          <p>120k</p>
+          <p>Cities</p>
+        </div>
 
-          <div class="statItem secondStatDiv">
-              <p>84k</p>
-              <p>Offices</p>
-          </div>
+        <div class="statItem secondStatDiv">
+          <p>84k</p>
+          <p>Offices</p>
+        </div>
 
-          <div class="statItem thirdStatDiv">
-              <p>34k</p>
-              <p>Delivery Vehicles</p>
-          </div>
+        <div class="statItem thirdStatDiv">
+          <p>34k</p>
+          <p>Delivery Vehicles</p>
+        </div>
       </div>
 
       <div class="gridMenu2">
-          <div class="firstGridMenu2">
-              <p>Rail Freight</p>
-          </div>
-          <div class="secondGridMenu2">
-              <p>Document & Parcel</p>
-          </div>
-          <div class="thirdGridMenu2">
-              <p>Road Freight</p>
-          </div>
-          <div class="fourthGridMenu2">
-              <p>Air Freight</p>
-          </div>
-          <div class="fifthGridMenu2">
-              <p>Discover</p>
-          </div>
-          <div class="sixthGridMenu2">
-              <p>Ocean Freight</p>
-          </div>
-          <div class="seventhGridMenu2">
-              <p>Shipping Service</p>
-          </div>
+        <div class="firstGridMenu2">
+          <p>Rail Freight</p>
+        </div>
+        <div class="secondGridMenu2">
+          <p>Document & Parcel</p>
+        </div>
+        <div class="thirdGridMenu2">
+          <p>Road Freight</p>
+        </div>
+        <div class="fourthGridMenu2">
+          <p>Air Freight</p>
+        </div>
+        <div class="fifthGridMenu2">
+          <p>Discover</p>
+        </div>
+        <div class="sixthGridMenu2">
+          <p>Ocean Freight</p>
+        </div>
+        <div class="seventhGridMenu2">
+          <p>Shipping Service</p>
+        </div>
       </div>
     </div>
 
     <div class="priContainer">
-        
+      <div class="subPriContainer"></div>
+      <p>Logistics <span>Solutions</span></p>
+      <div class="firstPriContainer">
+          <p>Warehouse Solutions</p>
+          <p>We have various warehouses across countries facilitating the storage of our clients packages.
+             we understand the critical role that warehousing plays in the logistics process. 
+             Our spacious warehouses are strategically located in key transportation hubs, 
+             allowing for easy access to major roads, ports, and airports. This enables us to offer cost-effective and timely distribution services to our clients.
+          </p>
+          <img alt="modelDhl-1" src="../assets/model1Dhl.png"/>
+          <div>
+              <p>DISCOVER</p>
+              <span class="material-symbols-sharp arrowEast"> north_east </span>
+          </div>
+      </div>
     </div>
   </div>
 </template>
